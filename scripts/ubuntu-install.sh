ad446d25618cf7d40f09b9b259b16bd7d89bffc0
Tom Allen <tswallen@outlook.com>
2022-05-02 13:44:23 +1000

    Latest

--- scripts/ubuntu-install.sh
+++ scripts/ubuntu-install.sh
@@ -1,1 +1,1 @@
-cp vlc-highlights.lua ~snap/vlc/2344/.local/share/vlc/lua/extensions
+cp vlc-highlights.lua ~/snap/vlc/2344/.local/share/vlc/lua/extensions
--- vlc-highlights.lua
+++ vlc-highlights.lua
@@ -1,19 +1,21 @@
-local dialog = nil
+local dlg = nil
 local highlights = {}
+local highlights_string = ''
 local input = nil
 local item = nil
 
-local player = nil
+local highlights_list = nil
+local file_name = ''
+local path = nil
 
-local current_time = nil
-
+-- VLC FUNCTIONS
 
 function descriptor()
 	return {
 		title = "Highlights";
 		version = "0.0.1";
 		author = "Tom Allen";
-		url = 'tba';
+		url = 'https://github.com/tswallen/vlc-highlights';
 		shortdesc = "Highlights: set time highlights in files.";
 		description = "Highlights will let you set highlights in your files and save them as .m3u files for later.";
 		capabilities = {"menu", "input-listener"}
@@ -23,18 +25,12 @@ end
 function activate()
 	input = vlc.object.input()
 	item = vlc.input.item()
-	dialog = vlc.dialog('Highlights')
-	open_dialog()
-end
-
-function open_dialog()
-	if item then
-		dialog:add_label('You have a file.')
-		dialog:add_label(get_current_time())
-	else
-		dialog:add_label('Please open a file to use this extension.')
-	end
-	dialog:show()
+	file_name = item:metas()['filename']
+	uri = item:uri()
+	path = vlc.strings.make_path(uri)
+	dlg = vlc.dialog('Highlights')
+	init_dialog()
+	dlg:show()
 end
 
 function close()
@@ -42,19 +38,62 @@ function close()
 end
 
 function deactivate()
-    if dialog then
-        dialog:hide()
+    if dlg then
+        dlg:hide()
     end
 end
 
-function close()
-	vlc.deactivate()
+-- INITIALISATION
+
+function init_dialog()
+	if item then
+		dlg:add_label('Current file: ' .. file_name)
+		dlg:add_label('Current URI: ' .. uri)
+		dlg:add_label('Current path: ' .. path)
+		dlg:add_button('Play/Pause', play_pause)
+		dlg:add_button('Add start', add_start)
+		dlg:add_button('Add stop', add_stop)
+		dlg:add_button('Save file', save_file)
+		highlights_list = dlg:add_list(2, 2, 1, 14)
+	else
+		dlg:add_label('Please open a file to use this extension.')
+	end
 end
+
+-- FUNCTIONS
 
 function play_pause()
 	vlc.playlist.pause()
 end
 
+function add_start()
+	start_string = '#EXTVLCOPT:start-time=' .. get_current_time()
+	highlights_list:add_value(start_string)
+	highlights_string = highlights_string .. start_string .. ' \n'
+end
+
+function add_stop()
+	stop_string = '#EXTVLCOPT:stop-time=' .. get_current_time()
+	highlights_list:add_value(stop_string)
+	highlights_list:add_value(file_name)
+	highlights_string = highlights_string .. stop_string .. ' \n'
+	highlights_string = highlights_string .. file_name .. ' \n'
+end
+
+function save_file()
+	os.execute('echo "' .. highlights_string .. '" > ~/Videos/test.m3u')
+end
+
+-- UTILS
+
 function get_current_time()
-    return vlc.var.get(input, 'time')
+	current_time_milliseconds = vlc.var.get(input, 'time')
+	current_time_milliseconds = current_time_milliseconds * 0.000001
+    return math.floor(current_time_milliseconds+0.5)
+end
+
+function set_highlights_string()
+	for k, highlight in highlights_list:get_selection() do highlights_string = highlights_string .. highlight .. ' \n' end
+	--for k, highlight in pairs(highlights) do highlights_string = highlights_string .. highlight .. '/n' end
+	for index, highlight in pairs(highlights) do highlights_list:add_value(highlight, index) end
 end
